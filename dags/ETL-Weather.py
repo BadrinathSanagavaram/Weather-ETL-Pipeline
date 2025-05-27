d2391c6f6e322b332f1d3662e62c2edd75a1cc77
BadrinathSanagavaram <badrinath.sanagavaram@gmail.com>
2025-05-27 01:30:51 -0400

    added the changes

--- dags/ETL-Weather.py
+++ dags/ETL-Weather.py
@@ -8,7 +8,7 @@ from airflow.utils.dates import days_ago                           ## used to cr
 LATITUDE = '51.5074'
 LONGITUDE= '-0.1278'
 POSTGRES_CONN_ID = 'postgres_default'
-API_CONN_API = 'open_meteo_api'
+API_CONN_ID = 'open_meteo_api'
 
 default_args = {
     'owner' : 'airflow',
